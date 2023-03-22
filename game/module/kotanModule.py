3a9546bdd3166921e043c44e116ef0a8ba9f133f
Abyss5057 <118429762+Abyss5057@users.noreply.github.com>
2023-03-22 19:39:52 +0900

    damedesu

--- game/module/kotanModule.py
+++ game/module/kotanModule.py
@@ -1,26 +1,44 @@
 import random
 
+numOpt = 4
+
 numExam = 10
-minQue = 0
+minQue = 1
 maxQue = 10
 
 examQueList = []
+examOptList = [[]]
 
-def shuffleListPlus(moto : list, jogai : list):
-    kansei = []
+def randomListGenerate(min, max, length, jogai = []):
+    if(max - min - len(jogai) + 1 < length):
+        return -1
 
-def prepareExam():
-    pass
+    output = []
+
+    for i in range(length):
+        kariNum = random.randint(min, max)
+
+        while(kariNum in output  or  kariNum in jogai):
+            kariNum = random.randint(min, max)
+        
+        output.append(kariNum)
+    
+    return output
+
+
+def prepareExam(min : int, max : int, num : int):
+    minQue = min
+    maxQue = max
+    numExam = num
 
 
 def makeExam():
-    for i in range(numExam):
-        temp = random.randint(minQue,maxQue)
-        if(temp in examQueList):
-            pass
-        else:
-            examQueList.append(temp)
+    examQueList = randomListGenerate(minQue, maxQue, numExam)
     
-    print(examQueList)
+    for i in range(numExam):
+        examOptList[i].append(randomListGenerate(minQue, maxQue, numOpt - 1, [examQueList[i]]))
+        random.shuffle(examOptList[i])
 
 makeExam()
+print(examQueList)
+print(examOptList)
